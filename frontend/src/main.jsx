991a487be2f4cd599d3ca374f3cc7bf03ff6c2ed
vedikasaklani <ivedikasaklani@gmail.com>
2026-06-03 21:40:50 +0530

    routing pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import LoginPage from './loginauth.jsx'
 import Projectpage from './project-page/projectlist.jsx'
+import SbomPage from './vuln-dash/vuln-dash.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <BrowserRouter>
   <Routes>
     <Route path="/" element={<LoginPage/>}/>
     <Route path="/projects" element={<Projectpage/>}/>
+    <Route path="/projects/:projectId/scans/:id" element={<SbomPage/>}/>
   </Routes>
   </BrowserRouter>
   </StrictMode>
