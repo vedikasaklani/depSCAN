136cd10bde82199e2cbeda5d4ac3b63c9f177245
vedikasaklani <ivedikasaklani@gmail.com>
2026-06-21 13:15:37 +0530

    merge main

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,8 @@ app = FastAPI(title="SBOM Backend")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173", 
+        "http://localhost:5173",
+         "http://localhost:5174",
     ],
     allow_credentials=True,
     allow_methods=["*"],
